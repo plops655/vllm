46c94b96ed6ea496c08ee25bcf11a0f0f042d9a3
Jayanth Sadhasivan <jayanthjns@gmail.com>
2025-02-26 11:28:48 -0800

    Added benchmark-latency CLI support

--- vllm/entrypoints/cli/openai.py
+++ vllm/entrypoints/cli/openai.py
@@ -10,16 +10,19 @@ import dataclasses
 import json
 import random
 import time
+from pathlib import Path
 from functools import cache
 
 from openai import OpenAI
 from openai.types.chat import ChatCompletionMessageParam
 
+from vllm import LLM, SamplingParams
 from vllm.entrypoints.cli.types import CLISubcommand
 from vllm.lora.request import LoRARequest
 from vllm.utils import FlexibleArgumentParser, merge_async_iterators
 
 import torch
+import numpy as np
 import uvloop
 from PIL import Image
 from tqdm import tqdm
@@ -29,11 +32,16 @@ from transformers import (AutoModelForCausalLM, AutoTokenizer,
 from vllm.engine.arg_utils import AsyncEngineArgs, EngineArgs
 from vllm.entrypoints.openai.api_server import (
     build_async_engine_client_from_engine_args)
-from vllm.inputs import TextPrompt
+from vllm.inputs import PromptType, TextPrompt
 from vllm.lora.utils import get_adapter_absolute_path
 from vllm.multimodal import MultiModalDataDict
 from vllm.sampling_params import BeamSearchParams
 from vllm.transformers_utils.tokenizer import AnyTokenizer, get_lora_tokenizer
+
+
+# Few changes:
+# Change type of prompt in SampleRequest to PromptType instead of str
+
 
 def _register_signal_handlers():
 
@@ -233,7 +241,7 @@ class SampleRequest:
             images).
         lora_request: Optional LoRARequest specifying the LoRA to use.
     """
-    prompt: str
+    prompt: PromptType
     prompt_len: int
     expected_output_len: int
     multi_modal_data: Optional[MultiModalDataDict] = None
@@ -382,7 +390,6 @@ class BenchmarkThroughputCommand(CLISubcommand):
             n: int,
             engine_args: EngineArgs,
     ) -> float:
-        from vllm import LLM, SamplingParams
         llm = LLM(**dataclasses.asdict(engine_args))
         assert all(
             llm.llm_engine.model_config.max_model_len >= (
@@ -567,7 +574,6 @@ class BenchmarkThroughputCommand(CLISubcommand):
         return end - start
 
     def cmd(self, args: argparse.Namespace) -> None:
-        model_name, client = _interactive_cli(args)
         print(args)
         import random
         random.seed(args.seed)
@@ -763,16 +769,154 @@ class BenchmarkThroughputCommand(CLISubcommand):
         return throughput_parser
 
 class BenchmarkLatencyCommand(CLISubcommand):
-    def cmd(args: argparse.Namespace) -> None:
-        pass
+    def cmd(self, args: argparse.Namespace) -> None:
+        print(args)
+
+        engine_args = EngineArgs.from_cli_args(args)
+
+        # NOTE(woosuk): If the request cannot be processed in a single batch,
+        # the engine will automatically process the request in multiple batches.
+        llm = LLM(**dataclasses.asdict(engine_args))
+        assert llm.llm_engine.model_config.max_model_len >= (
+                args.input_len +
+                args.output_len), ("Please ensure that max_model_len is greater than"
+                                   " the sum of input_len and output_len.")
+
+        sampling_params = SamplingParams(
+            n=args.n,
+            temperature=1.0,
+            top_p=1.0,
+            ignore_eos=True,
+            max_tokens=args.output_len,
+        )
+        print(sampling_params)
+        dummy_prompt_token_ids = np.random.randint(10000,
+                                                   size=(args.batch_size,
+                                                         args.input_len))
+        dummy_prompts: List[PromptType] = [{
+            "prompt_token_ids": batch
+        } for batch in dummy_prompt_token_ids.tolist()]
+
+        def llm_generate():
+            if not args.use_beam_search:
+                llm.generate(dummy_prompts,
+                             sampling_params=sampling_params,
+                             use_tqdm=False)
+            else:
+                llm.beam_search(
+                    dummy_prompts,
+                    BeamSearchParams(
+                        beam_width=args.n,
+                        max_tokens=args.output_len,
+                        ignore_eos=True,
+                    ),
+                )
+
+        def run_to_completion(profile_dir: Optional[str] = None):
+            if profile_dir:
+                with torch.profiler.profile(
+                        activities=[
+                            torch.profiler.ProfilerActivity.CPU,
+                            torch.profiler.ProfilerActivity.CUDA,
+                        ],
+                        on_trace_ready=torch.profiler.tensorboard_trace_handler(
+                            str(profile_dir)),
+                ) as p:
+                    llm_generate()
+                print(p.key_averages().table(sort_by="self_cuda_time_total"))
+            else:
+                start_time = time.perf_counter()
+                llm_generate()
+                end_time = time.perf_counter()
+                latency = end_time - start_time
+                return latency
+
+        print("Warming up...")
+        for _ in tqdm(range(args.num_iters_warmup), desc="Warmup iterations"):
+            run_to_completion(profile_dir=None)
+
+        if args.profile:
+            profile_dir = args.profile_result_dir
+            if not profile_dir:
+                profile_dir = (Path(".") / "vllm_benchmark_result" /
+                               f"latency_result_{time.time()}")
+            print(f"Profiling (results will be saved to '{profile_dir}')...")
+            run_to_completion(profile_dir=profile_dir)
+            return
+
+        # Benchmark.
+        latencies = []
+        for _ in tqdm(range(args.num_iters), desc="Profiling iterations"):
+            latencies.append(run_to_completion(profile_dir=None))
+        latencies = np.array(latencies)
+        percentages = [10, 25, 50, 75, 90, 99]
+        percentiles = np.percentile(latencies, percentages)
+        print(f"Avg latency: {np.mean(latencies)} seconds")
+        for percentage, percentile in zip(percentages, percentiles):
+            print(f"{percentage}% percentile latency: {percentile} seconds")
+
+        # Output JSON results if specified
+        if args.output_json:
+            results = {
+                "avg_latency": np.mean(latencies),
+                "latencies": latencies.tolist(),
+                "percentiles": dict(zip(percentages, percentiles.tolist())),
+            }
+            with open(args.output_json, "w") as f:
+                json.dump(results, f, indent=4)
+            save_to_pytorch_benchmark_format(args, results)
 
     def subparser_init(
             self,
             subparsers: argparse._SubParsersAction) -> FlexibleArgumentParser:
-        pass
+        subparsers.add_argument("--input-len", type=int, default=32)
+        subparsers.add_argument("--output-len", type=int, default=128)
+        subparsers.add_argument("--batch-size", type=int, default=8)
+        subparsers.add_argument(
+            "--n",
+            type=int,
+            default=1,
+            help="Number of generated sequences per prompt.",
+        )
+        subparsers.add_argument("--use-beam-search", action="store_true")
+        subparsers.add_argument(
+            "--num-iters-warmup",
+            type=int,
+            default=10,
+            help="Number of iterations to run for warmup.",
+        )
+        subparsers.add_argument("--num-iters",
+                            type=int,
+                            default=30,
+                            help="Number of iterations to run.")
+        subparsers.add_argument(
+            "--profile",
+            action="store_true",
+            help="profile the generation process of a single batch",
+        )
+        subparsers.add_argument(
+            "--profile-result-dir",
+            type=str,
+            default=None,
+            help=("path to save the pytorch profiler output. Can be visualized "
+                  "with ui.perfetto.dev or Tensorboard."),
+        )
+        subparsers.add_argument(
+            "--output-json",
+            type=str,
+            default=None,
+            help="Path to save the latency results in JSON format.",
+        )
+
+        latency_parser = EngineArgs.add_cli_args(subparsers)
 
 class BenchmarkServingCommand(CLISubcommand):
-    pass
+    def cmd(args: argparse.Namespace) -> None:
+        pass
+    def subparser_init(
+            self,
+            subparsers: argparse._SubParsersAction) -> FlexibleArgumentParser:
+        pass
 
 def cmd_init() -> List[CLISubcommand]:
     return [ChatCommand(), CompleteCommand(), BenchmarkThroughputCommand()]
